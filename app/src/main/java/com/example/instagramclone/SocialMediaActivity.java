9db9bda69c519e203dbd172baf39783adb16fe65
12coldest <mpprince@hotmail.com>
2020-04-11 11:09:58 -0400

    This is the end of Chapter 86 before migrate to android X

---
+++ app/src/main/java/com/example/instagramclone/SocialMediaActivity.java
@@ -0,0 +1,29 @@
+package com.example.instagramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class SocialMediaActivity extends AppCompatActivity {
+    private android.support.v7.widget.Toolbar toolbar;
+        
+    private ViewPager wiewPager;
+    private TabLayout tabLayout;
+    private TabAdapter tabAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_media);
+
+
+
+    }
+
+
+
+}
